97f74a035578e78de5a69af37048b146c055ba57
Henrik Mikkelsen <henrik@leadout.dk>
2019-09-12 23:08:11 +0200

    Add PDF checkboxes

---
+++ src/PDF/Elements/Checkbox.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Leadout\Reports\PDF\Elements;
+
+use setasign\Fpdi\Fpdi;
+
+class Checkbox implements Contract
+{
+    /**
+     * The values that the checkbox has.
+     *
+     * @var array
+     */
+    private $values;
+
+    /**
+     * The choices that this checkbox has.
+     *
+     * @var array
+     */
+    private $choices;
+
+    /**
+     * Instantiate the checkbox.
+     *
+     * @param mixed $values the values that the checkbox has.
+     */
+    public function __construct($values = null)
+    {
+        $this->values = is_array($values) ? $values : func_get_args();
+
+        $this->choices = collect();
+    }
+
+    /**
+     * Instantiate a new checkbox.
+     *
+     * @param mixed $values the values that the checkbox has.
+     * @return Checkbox the new checkbox.
+     */
+    public static function create($values = null)
+    {
+        return new static(is_array($values) ? $values : func_get_args());
+    }
+
+    /**
+     * Render the element onto the given PDF.
+     *
+     * @param FPDI $pdf the PDF.
+     * @return void
+     */
+    public function render($pdf)
+    {
+        $this->choices->each->render($pdf, $this->values);
+    }
+
+    public function addChoice($value, $x, $y)
+    {
+        $this->choices->push(Choice::create($value, $x, $y));
+
+        return $this;
+    }
+}
---
+++ src/PDF/Elements/Choice.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Leadout\Reports\PDF\Elements;
+
+use setasign\Fpdi\Fpdi;
+
+class Choice
+{
+    /**
+     * The value of the choice.
+     *
+     * @var string
+     */
+    private $value;
+
+    /**
+     * The X coordinate of the choice on the page.
+     *
+     * @var float
+     */
+    private $x;
+
+    /**
+     * The Y coordinate of the choice on the page.
+     *
+     * @var float
+     */
+    private $y;
+
+    /**
+     * Instantiate the class and set the properties.
+     *
+     * @param string $value the value of the choice.
+     * @param float  $x     the X coordinate of the choice on the page.
+     * @param float  $y     the Y coordinate of the choice on the page.
+     */
+    public function __construct($value, $x, $y)
+    {
+        $this->value = $value;
+
+        $this->x = $x;
+
+        $this->y = $y;
+    }
+
+    /**
+     * Instantiate a new choice.
+     *
+     * @param string $value the value of the choice.
+     * @param float  $x     the X coordinate of the choice on the page.
+     * @param float  $y     the Y coordinate of the choice on the page.
+     * @return Choice the new choice.
+     */
+    public static function create($value, $x, $y)
+    {
+        return new static($value, $x, $y);
+    }
+
+    /**
+     * Render this choice onto the given PDF.
+     *
+     * @param FPDI  $pdf    the PDF.
+     * @param array $values the values that are chosen in the parent checkbox.
+     * @return void
+     */
+    public function render($pdf, $values)
+    {
+        $pdf->Image($this->getPath($values), $this->getX(), $this->getY(), $this->getWidth(), $this->getHeight());
+    }
+
+    private function getPath($values)
+    {
+        return '../../../img/' . (in_array($this->value, $values, true) ? 'checked-new.png' : 'unchecked-new.png');
+    }
+
+    private function getX()
+    {
+        return $this->x;
+    }
+
+    private function getY()
+    {
+        return $this->y;
+    }
+
+    private function getWidth()
+    {
+        return 3.5;
+    }
+
+    private function getHeight()
+    {
+        return 3.5;
+    }
+}
