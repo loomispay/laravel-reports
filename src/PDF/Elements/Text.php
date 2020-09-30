c83903e279bdaee991d2d38633e0bae8cd4bca2c
Henrik Romby Mikkelsen <henrik@leadout.dk>
2020-09-30 10:27:30 +0200

    Add alignment to PDF text cells

--- src/PDF/Elements/Text.php
+++ src/PDF/Elements/Text.php
@@ -42,15 +42,23 @@ class Text implements Contract
     private $height;
 
     /**
+     * The alignment of the text.
+     *
+     * @var string
+     */
+    private $align;
+
+    /**
      * Instantiate the text.
      *
-     * @param string $content  the content of the text field.
-     * @param int    $x      the leftmost x coordinate of the text.
-     * @param int    $y      the topmost y coordinate of the text.
-     * @param int    $width  the width of the text.
-     * @param int    $height the height of the text.
+     * @param string $content the content of the text field.
+     * @param int    $x       the leftmost x coordinate of the text.
+     * @param int    $y       the topmost y coordinate of the text.
+     * @param int    $width   the width of the text.
+     * @param int    $height  the height of the text.
+     * @param string $align   the alignment of the text.
      */
-    public function __construct($content, $x, $y, $width = 0, $height = 0)
+    public function __construct($content, $x, $y, $width = 0, $height = 0, $align = '')
     {
         $this->content = $content;
 
@@ -61,21 +69,48 @@ class Text implements Contract
         $this->width = $width;
 
         $this->height = $height;
+
+        $this->align = $align;
     }
 
     /**
      * Make a new text.
      *
-     * @param string $content  the content of the text field.
-     * @param int    $x      the leftmost x coordinate of the text.
-     * @param int    $y      the topmost y coordinate of the text.
-     * @param int    $width  the width of the text.
-     * @param int    $height the height of the text.
+     * @param string $content the content of the text field.
+     * @param int    $x       the leftmost x coordinate of the text.
+     * @param int    $y       the topmost y coordinate of the text.
+     * @param int    $width   the width of the text.
+     * @param int    $height  the height of the text.
+     * @param string $align   the alignment of the text.
      * @return Text the text.
      */
-    public static function make($content, $x, $y, $width = 0, $height = 0)
+    public static function make($content, $x, $y, $width = 0, $height = 0, $align = '')
     {
-        return new Text($content, $x, $y, $width, $height);
+        return new Text($content, $x, $y, $width, $height, $align);
+    }
+
+    /**
+     * Center align the text.
+     *
+     * @return $this
+     */
+    public function center()
+    {
+        $this->align = 'C';
+
+        return $this;
+    }
+
+    /**
+     * Right align the text.
+     *
+     * @return $this
+     */
+    public function right()
+    {
+        $this->align = 'R';
+
+        return $this;
     }
 
     /**
@@ -88,6 +123,6 @@ class Text implements Contract
     {
         $pdf->SetXY($this->x, $this->y);
 
-        $pdf->Multicell($this->width, $this->height, utf8_decode($this->content), 0, '');
+        $pdf->Multicell($this->width, $this->height, utf8_decode($this->content), false, $this->align);
     }
 }
